5738122968b028bda15a6035280c4a8305c04cab
Pedro Gabriel Ferreira Franco <78420843+RetroIA@users.noreply.github.com>
2023-09-22 15:44:31 -0300

    Pull request
    Criei uma solicitação de pull de acordo com o pedido em:
    "Open a pull request and let your teacher know that you’ve finished this course."

---
+++ Finalizado.java
@@ -0,0 +1,10 @@
+/* 
+Nome: Pedro Gabriel
+Email: pedrogffranco@aluno.uespi.br
+*/
+
+public class Finalizado {
+    public static void main(String[] args) {
+        System.out.println("Tarefa finalizada.");
+    }
+}
